09e511bd829635c45bcead1b109b2299a7dfc1b6
Prathyusha-konda <prathyushakonda81@gmail.com>
2016-08-30 15:09:19 -0300

    Add files via upload

---
+++ SpectMHC_main.py
@@ -0,0 +1,77 @@
+import SpectMHC_methods
+
+if SpectMHC_methods.install_check("verify") in 'yes':
+	print "\nOk, Let's move ahead\n"
+	version = SpectMHC_methods.version("verify version")
+	split_output=raw_input("\nDo you want to split your parent file? yes or no: ")
+	if split_output in 'yes':
+		split_list=SpectMHC_methods.split_files("split")
+		check1 = raw_input("\nDo you want to save your splitfiles after we complete the execution? yes or no: ")
+		check2 = raw_input("\nDo you want the output in fasta format? note: this includes processing after netMHC output, yes or no: ") 
+			
+
+		if check2 in 'yes':
+			if version in '3.4':
+				cut_off=float(raw_input("\nWhat is the binding affinity cut_off you are interested in? netmhc suggests 50 for strong binders and 500 for weak binders: "))
+			else:
+				cut_off=float(raw_input("\nWhat is the cut_off Rank you are interested in? netMHC suggests cutoff rank of 0.5 for strong binders and 2 for weak binders: "))			
+
+			check3 = raw_input("\nDo you want to save your raw netMHC output files along with the fasta output? yes or no: ")
+
+			outfile_list=SpectMHC_methods.executemhc(version, split_list)
+  
+			print "\nWait while your data is processed into fasta format..."
+			if check1 in 'no':
+				SpectMHC_methods.del_temp_files(split_list)
+			elif check1 in 'yes':
+				print "\nSplit files are saved to your computer..."
+
+			if check2 in 'yes':
+				SpectMHC_methods.process_data(version, outfile_list, cut_off)
+
+			if check3 in 'yes':
+				print "\nThe execution is complete. Check your output files in the folder..."
+			elif check3 in 'no':
+				SpectMHC_methods.del_temp_files(outfile_list)
+
+		elif check2 in 'no':
+			
+			SpectMHC_methods.executemhc(version, split_list)
+	
+			if check1 in 'no':
+				SpectMHC_methods.del_temp_files(split_list)
+			elif check1 in 'yes':
+				print "\nSplit files are saved to your computer..."		
+ 
+
+	elif split_output in 'no':
+		files = raw_input("Please enter your file names, if you have multiple files, separate them by space. ex- 1.fsa 2.fsa 3.fsa: ")
+		input_files = files.split()
+			
+		
+		check2 = raw_input("Do you want the output in fasta format? yes or no: ") 
+		if check2 in 'yes':
+			if version in '3.4':
+				cut_off=float(raw_input("\nWhat is the binding affinity cut_off you are interested in? netmhc suggests 50 for strong binders and 500 for weak binders: "))
+			else:
+				cut_off=float(raw_input("\nWhat is the cut_off Rank you are interested in? netMHC suggests cutoff rank of 0.5 for strong binders and 2 for weak binders : "))			
+			check3 = raw_input("\nDo you want to save your raw netMHC output files along with the fasta output? yes or no: ")
+			file_list=SpectMHC_methods.executemhc(version, input_files)
+		
+			print "\nWait while your data is processed into fasta format"
+			
+			if check2 in 'yes':			
+				SpectMHC_methods.process_data(version, file_list, cut_off)
+			
+			if check3 in 'yes':
+				print "\nThe execution is complete. Check your output files in the folder."
+			elif check3 in 'no':
+				SpectMHC_methods.del_temp_files(file_list)
+				
+
+		elif check2 in 'no':
+			SpectMHC_methods.executemhc(version, input_files)
+		
+			
+	
+			
---
+++ SpectMHC_methods.py
@@ -0,0 +1,153 @@
+
+
+def install_check(check):
+	print "check"
+
+	netMHC = raw_input('\nDid you install netMHC? reply with yes or no:')
+
+	choice = netMHC.lower()
+	
+	return choice;
+
+
+def version(check): 
+	print "check"
+	
+	version = raw_input('\nWhich version of netMHC do you want to use? 4.0 or 3.4 or pan: ')
+
+	if version in ['4.0']:
+		print "4.0 is your version"
+		
+	elif version in ['pan']:
+		print "netMHCpan is your version"
+	
+	elif version in ['3.4']:
+		print "3.4 is your version"
+	
+	return version;
+
+
+def split_files(split):
+	def chunks(l, n):
+    		n = max(1, n)
+    		return [l[i:i + n] for i in range(0, len(l), n)]
+	def new_string(x):
+		temp = ">"
+		for i in x:
+			temp = temp+i+">"
+		temp = temp[:-1]
+		return temp
+	filename = raw_input("\nEnter file name: ")
+	number = int(raw_input("\nHow many number of sequences do you want per file? : "))
+	split_list=[]
+
+
+	def mkfile(x,i):
+		f = open("split"+str(i)+"_"+filename,"w")
+		splitfile='split%s_%s' %(str(i), filename)
+		split_list.append(splitfile)
+		f.write(x)
+		f.close()
+	
+	
+
+	f = open(filename, "r")
+	splitted_file = f.read().split(">")
+
+	new_files = chunks(splitted_file, number)
+
+	count = 1
+	for i in new_files:
+		q = new_string(i)
+		if count == 1:
+			q = q[1:]
+		mkfile(q,count)
+		count=count+1
+
+	return split_list
+
+
+def executemhc(version, list_of_files):
+	
+	import datetime
+	print datetime.datetime.now()
+	import glob, os
+	import threading
+	
+	path = raw_input("\nPlease enter the path to netMHC folder, ex: /home/prathyusha/NetMHC_4.0_for_Linux/netMHC-4.0: ")
+	
+	num = raw_input("\nPlease enter which length peptides you want to predict; if multiple lengths, seperate numbers with space 8 9 10 11: ")
+	input_list = num.split()
+	input_list = [int(a) for a in input_list]
+
+
+	allele=raw_input("\nPlease enter the allele you want to predict; ex- H-2-Kb or HLA-A0301: ")
+
+	out_list = []
+	for i in input_list:	
+		for filename in list_of_files: 
+
+			head, sep, tail = filename.partition('.f')
+			print '\nfile %s is executed' %head 
+			
+			outfile='%s_%d_output.txt' %(head, i)
+			out_list.append(outfile)
+			
+			if version in '4.0':
+				command = '%s/netMHC -l %d -a %s %s > %s' %(path, i, allele, filename, outfile)
+			elif version in 'pan':
+				command = '%s/netMHCpan -l %d -a %s %s > %s_%d_output.txt' %(path, i, allele, filename, outfile)			
+			elif version in '3.4':
+				command = '%s/netMHC -l %d -a %s %s > %s' %(path, i, allele, filename, outfile)
+			os.system(command)
+
+	print "\nnetMHC execution completed"
+	
+	return out_list
+
+
+def process_data(version, filelist, cut_off):
+	
+	if version in '4.0':
+		bad_words = ['high binders', 'iCore', '----', '#']
+	
+	elif version in 'pan':
+		bad_words = ['#', 'training data', 'ICore', 'high binders', '------']
+
+	elif version in '3.4':
+		bad_words = ['binder threshold', 'Artificial Neural', 'affinity(nM)', '--------', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+	for filename in filelist:
+		newname=filename.replace("output","data")
+		print filename
+		with open(filename) as oldfile, open(newname, 'w') as newfile:
+			for line in oldfile:
+				if not any(bad_word in line for bad_word in bad_words):
+					if line.strip():
+						newline=line.split()
+						if version in '3.4':
+							if float(newline[3])<cut_off:
+								if cut_off<500:
+									newfile.write(">>"+newline[5]+"_"+newline[3]+"_"+newline[6]+'\n'+newline[1]+'\n')
+								else:
+									newfile.write(">>"+newline[4]+"_"+newline[3]+"_"+newline[5]+'\n'+newline[1]+'\n')
+
+						else:
+
+							if float(newline[13])<cut_off:
+								newfile.write(">>"+newline[10]+"_"+newline[13]+"_"+newline[1]+'\n'+newline[2]+'\n')
+
+	
+
+	print "\nnetMHC processing complete"
+
+
+
+def del_temp_files(list_of_temp_files):
+	import os
+	for filename in list_of_temp_files:
+		os.remove(filename)
+	print "\nThe execution is complete. Check your output files in the folder."
+
+
+
